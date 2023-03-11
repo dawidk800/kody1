8cd72bbd4508fded60af78474e9a657a7f1612a8
dawidk800 <127591456+dawidk800@users.noreply.github.com>
2023-03-11 11:51:43 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       // int name;
+        //name = 10;
+        Scanner odczyt = new Scanner(System.in);
+      //  System.out.println(name);
+
+        String name = odczyt.nextLine();
+
+
+
+
+
+
+
+    }
+
+
+}
