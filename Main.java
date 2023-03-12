50f759f4df9e46dade28c4a6a13bc53c43cc234b
dawidk800 <127591456+dawidk800@users.noreply.github.com>
2023-03-12 11:50:42 +0100

    zajęcia 12.03.2023

--- Main.java
+++ Main.java
@@ -4,18 +4,36 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("ile masz lat?");
         Scanner skan = new Scanner(System.in);
-        String name = skan.nextLine();
 
-        System.out.println("Masz " + name + " lat");
+
         System.out.println("Jak się nazywasz?");
         String nazwa = skan.nextLine();
         System.out.println("Masz na imię " + nazwa);
 
+        System.out.println("ile masz lat?");
+        int wiek = skan.nextInt();
+
+        System.out.println("Masz " + wiek + " lat");
+        if (wiek > 18) {
+            System.out.println("Jesteś pełnoletni");
+        } else {
+            System.out.println("jesteś niepełnoletni");
+        }
+
+        System.out.println("jaki jest twój wzrost?");
+        double wzrost = skan.nextDouble();
+
+        if (wzrost <= 160) {
+            System.out.println("jesteś bardzo niksi");
+        } else if (wzrost >= 160 && wzrost <= 170) {
+            System.out.println("jesteś niski");
+        } else if (wzrost >= 170 && wzrost <= 180) {
+            System.out.println("jesteś wysoki");
+        } else {
+            System.out.println("jesteś bardzo wysoki");
+        }
+
 
     }
-
-
-
-    }
+}
